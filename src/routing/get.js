be6e2e10ad3d564f3d36b1466179a9766967354b
Raffrerty <raffsimms23@gmail.com>
2021-10-02 14:06:01 +0100

    fin.

--- src/routing/get.js
+++ src/routing/get.js
@@ -5,14 +5,37 @@ const fs = require('fs');
 
 const publicDirectory = path.join(__dirname, '../../public');
 
+function getAvailableFonts() {
+    return fs.readdirSync(path.join(publicDirectory, "fonts"));
+}
+
 router.get("/", (req, res) => {
     if (req.query.font) {
         res.sendFile(`fonts/${req.query.font}/stylesheet.css`, { root: publicDirectory });
     } else {
-        res.render("index", {
-            fonts: fs.readdirSync(path.join(publicDirectory, "fonts"))
+        return res.render("index", {
+            fonts: getAvailableFonts()
         });
     }
 });
+
+router.get("/preview", (req, res) => {
+    if (req.query.font) {
+        if (getAvailableFonts().includes(req.query.font)) {
+            const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const lower = "abcdefghijklmnopqrstuvwxyz";
+            const numbers = "1234567890"
+            
+            return res.render("preview", {
+                font: req.query.font,
+                upper_case: upper, 
+                lower_case: lower,
+                numbers: numbers
+            })
+        }
+    }
+    return res.redirect("/");
+});
+
 
 module.exports = router;
